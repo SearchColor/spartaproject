d43a06bffbd726750034e1773e4a5d69ddd823fd
searchcolor <qkrdydwo1022@gmail.com>
2024-10-14 17:11:11 +0900

    Feat : Add class
    4주차 강의 class 예제

---
+++ src/week04/OurClass.java
@@ -0,0 +1,12 @@
+package week04;
+
+public class OurClass {
+    private final boolean just = true;
+
+    // throws : 던지다(예외를 던지다)
+
+    public void thisMethodIsDangerous () throws OurbadException {
+        //custom logic
+
+    }
+}
---
+++ src/week04/OurbadException.java
@@ -0,0 +1,7 @@
+package week04;
+
+public class OurbadException extends Exception{
+    public OurbadException (){
+        super("위험한 행동을 하면 예외처리를 꼭 해야함!");
+    }
+}
